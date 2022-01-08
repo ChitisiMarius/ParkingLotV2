df131471c98aba826ade7dd79891d9f24196c10e
ChitisiMarius <chitisimarius9@gmail.com>
2022-01-08 06:59:32 +0200

    app5, user and car entity classes + database

---
+++ ParkingLot/src/main/java/com/park/parkinglot/entity/User.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.park.parkinglot.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Marius
+ */
+@Entity
+@Table(name = "USERS")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Integer id;
+
+    private String username;
+    private String email;
+    private String password;
+    private String position;
+
+    @OneToMany(mappedBy = "user")
+    private Collection<Car> cars;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
